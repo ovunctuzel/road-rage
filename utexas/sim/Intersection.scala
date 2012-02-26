5c93c4f59908ac50f1b8338dc9bd5e420e0b017e
Dustin Carlino <dabreegster@gmail.com>
2012-02-26 11:09:30 -0600

    autonomous reservation policy's gridlock also eased

--- utexas/sim/Intersection.scala
+++ utexas/sim/Intersection.scala
@@ -1,7 +1,9 @@
 package utexas.sim
 
 import scala.collection.mutable.{HashMap => MutableMap}
-import scala.collection.mutable.{HashSet => MutableSet}
+import scala.collection.mutable.MultiMap
+import scala.collection.mutable.{Set => MutableSet}
+import scala.collection.mutable.{HashSet => MutableHashSet}
 import scala.collection.mutable.ListBuffer
 import scala.collection.immutable.{SortedSet, TreeSet}
 
@@ -12,9 +14,9 @@ import utexas.{Util, cfg}
 // Reason about collisions from conflicting simultaneous turns.
 class Intersection(val v: Vertex) {
   //val policy: Policy = new NeverGoPolicy(this)
-  val policy: Policy = new StopSignPolicy(this)
+  //val policy: Policy = new StopSignPolicy(this)
   //val policy: Policy = new SignalCyclePolicy(this)
-  //val policy: Policy = new ReservationPolicy(this)
+  val policy: Policy = new ReservationPolicy(this)
 
   override def toString = "Intersection(" + v + ")"
 
@@ -162,39 +164,9 @@ class ReservationPolicy(intersection: Intersection) extends Policy(intersection)
   var current_batch = new TurnBatch()
   var reservations = List[TurnBatch]()
   // used to determine if it's an agent's first requent or not
-  val current_agents = new MutableSet[Agent]()
+  val current_agents = new MutableHashSet[Agent]()
 
-  def can_go(a: Agent, turn: Turn, far_away: Double): Boolean = {
-    val first_req = !current_agents.contains(a)
-
-    if (first_req) {
-      current_agents += a
-      if (current_batch.add_turn(turn)) {
-        return true
-      } else {
-        // A conflicting turn. Add it to the reservations.
-
-        // Is there an existing batch of reservations that doesn't conflict?
-        if (!reservations.find(r => r.add_turn(turn)).isDefined) {
-          // new batch!
-          val batch = new TurnBatch()
-          batch.add_turn(turn)
-          reservations :+= batch
-        }
-
-        return false
-      }
-    } else {
-      return current_batch.has_turn(turn)
-    }
-  }
-
-  def validate_entry(a: Agent, turn: Turn) = current_batch.has_turn(turn)
-
-  def handle_exit(a: Agent, turn: Turn) = {
-    assert(current_batch.has_turn(turn))
-    current_batch.remove_turn(turn)
-    current_agents -= a
+  def shift_batches() = {
     if (current_batch.all_done) {
       // Time for the next reservation! If there is none, then keep
       // current_batch because it's empty anyway.
@@ -205,29 +177,83 @@ class ReservationPolicy(intersection: Intersection) extends Policy(intersection)
     }
   }
 
-  override def current_greens = current_batch.turns.keys.toSet
+  def can_go(a: Agent, turn: Turn, far_away: Double): Boolean = {
+    val first_req = !current_agents.contains(a)
+
+    // check everyone in the current batch. if any are not moving and not in their
+    // turn, then cancel them -- they're almost definitely stuck behind somebody
+    // who wants to do something different.
+    // TODO alternative is to not schedule them until there's nobody else in front
+    // trying to do something different. this is hard because agents don't call us
+    // in any particular order.
+    current_agents --= current_batch.flush_stalled
+    shift_batches
+
+    if (first_req) {
+      current_agents += a
+      if (current_batch.add_ticket(a, turn)) {
+        return true
+      } else {
+        // A conflicting turn. Add it to the reservations.
+
+        // Is there an existing batch of reservations that doesn't conflict?
+        if (!reservations.find(r => r.add_ticket(a, turn)).isDefined) {
+          // new batch!
+          val batch = new TurnBatch()
+          batch.add_ticket(a, turn)
+          reservations :+= batch
+        }
+
+        return false
+      }
+    } else {
+      return current_batch.has_ticket(a, turn)
+    }
+  }
+
+  def validate_entry(a: Agent, turn: Turn) = current_batch.has_ticket(a, turn)
+
+  def handle_exit(a: Agent, turn: Turn) = {
+    assert(current_batch.has_ticket(a, turn))
+    current_batch.remove_ticket(a, turn)
+    current_agents -= a
+    shift_batches
+  }
+
+  override def current_greens = current_batch.tickets.keys.toSet
 
   def unregister(a: Agent) = {
     if (current_agents.contains(a)) {
-      // TODO what turn do they want to do? decrement that counter...
+      // TODO what turn do they want to do? nuke-agent doesn't work till we get
+      // this right.
     }
+  }
+
+  override def dump_info() = {
+    Util.log(reservations.size + " reservations pending")
+    Util.log("Currently:")
+    Util.log_push
+    for (t <- current_batch.tickets.keys) {
+      Util.log(t + ": " + current_batch.tickets(t))
+    }
+    Util.log_pop
   }
 }
 
-// Count how many agents are doing each type of turn, and add turns that don't
+// Count what agents are doing each type of turn, and add turns that don't
 // conflict
 class TurnBatch() {
-  val turns = new MutableMap[Turn, Int]
+  val tickets = new MutableMap[Turn, MutableSet[Agent]] with MultiMap[Turn, Agent]
 
   // false if it conflicts with this group
-  def add_turn(t: Turn): Boolean = {
-    if (turns.contains(t)) {
+  def add_ticket(a: Agent, t: Turn): Boolean = {
+    if (tickets.contains(t)) {
       // existing turn
-      turns(t) += 1
+      tickets.addBinding(t, a)
       return true
-    } else if (turns.keys.filter(c => t.conflicts(c)).size == 0) {
+    } else if (tickets.keys.filter(c => t.conflicts(c)).size == 0) {
       // new turn that doesn't conflict
-      turns(t) = 1
+      tickets.addBinding(t, a)
       return true
     } else {
       // conflict
@@ -235,16 +261,27 @@ class TurnBatch() {
     }
   }
 
-  def has_turn(t: Turn) = turns.contains(t)
-
-  def remove_turn(t: Turn) = {
-    turns(t) -= 1
-    if (turns(t) == 0) {
-      turns -= t
+  def flush_stalled(): MutableSet[Agent] = {
+    val canceled = new MutableHashSet[Agent]()
+    // Nix agents who haven't started turn and are not moving.
+    for (t <- tickets.keys) {
+      for (a <- tickets(t)) {
+        if (a.at.on != t && a.speed == 0.0) {
+          remove_ticket(a, t)
+          canceled += a
+        }
+      }
     }
+    return canceled
   }
 
-  def all_done = turns.isEmpty
+  def has_ticket(a: Agent, t: Turn) = tickets.contains(t) && tickets(t).contains(a)
+
+  def remove_ticket(a: Agent, t: Turn) = {
+    tickets.removeBinding(t, a)
+  }
+
+  def all_done = tickets.isEmpty
 }
 
 // A cycle-based light.
@@ -259,7 +296,7 @@ class SignalCyclePolicy(intersection: Intersection) extends Policy(intersection)
   // a turn until too late..
   // these are the agents we've _accepted_, whether or not they've started their
   // turn.
-  val current_agents = new MutableSet[(Agent, Turn)]
+  val current_agents = new MutableHashSet[(Agent, Turn)]
   // also gives us our state
   var start_waiting: Option[Double] = None
   
@@ -278,7 +315,7 @@ class SignalCyclePolicy(intersection: Intersection) extends Policy(intersection)
     var turns_left: SortedSet[Turn] = new TreeSet[Turn] ++ intersection.v.turns
     val cycle_list = new ListBuffer[Set[Turn]]()
     while (!turns_left.isEmpty) {
-      val this_group = new MutableSet[Turn]()
+      val this_group = new MutableHashSet[Turn]()
       val canonical = turns_left.head
       this_group += canonical
       turns_left = turns_left.tail
