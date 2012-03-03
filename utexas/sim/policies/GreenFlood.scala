e1f6123126c6cb8a27cb1cdc3e4d6e8da6f83993
Dustin Carlino <dabreegster@gmail.com>
2012-03-03 10:17:18 -0600

    fixing greenflood to have legal timings

--- utexas/sim/policies/GreenFlood.scala
+++ utexas/sim/policies/GreenFlood.scala
@@ -14,56 +14,50 @@ import utexas.Util
 
 // the name comes from http://en.wikipedia.org/wiki/Green_wave
 class GreenFlood(sim: Simulation) {
-  val stdPhases = sim.vertices.map(v => (v, Cycle.grouped_left_turn_sets(v))).toMap
+  // output
   val cycles = sim.vertices.map(v => (v, new ListBuffer[Cycle]())).toMap
 
-  // TODO non-deterministic results!!! sorted set implementation is SLOW though.
   val visited = new MutableSet[Vertex]()
 
   def compute(): Map[Vertex, ListBuffer[Cycle]] = {
+    // begin flooding at a critical intersection
     val start_at = sim.vertices.maxBy[Double](v => v.get_priority).in_edges.head
-    Util.log("Starting flood from "+start_at)
-    var startCycles = getCycles(start_at, 0) //Cycle.cycle_for_edge(start_at, 0, duration)
-    cycles(start_at.to) ++= startCycles
+    Util.log("Starting flood from " + start_at)
+    // schedule first intersection
+    cycles(start_at.to) ++= schedule(start_at, 0)
+    // do the main work
     flood(start_at.to)
-
-    // expand
+    // expand turns
     cycles.values.flatten.foreach(c => c.expand_all_turns)
     
     return cycles
   }
   
-  def getCycles(edge: Edge, offset: Double):Set[Cycle] = {
-    val preCycleSet = stdPhases(edge.to)
-    //First do the incoming edge
-    var desiredCycle = Set[Turn]()
-    for (preCycle <- preCycleSet){
-      if (preCycle.contains(edge.leads_to.head)) desiredCycle = preCycle
-    }
-    assert(!desiredCycle.isEmpty)
-    val c = new Cycle(offset, (cfg.sig_duration/preCycleSet.size.toDouble).toInt)
-    for (turn <- desiredCycle) c.add_turn(turn)
-    //Now do the rest of the cycles
-    var cycleSet = Set[Cycle]()
-    cycleSet += c
-    var i = 1
-    for (preCycle <- preCycleSet if preCycle != desiredCycle){
-      val oc = new Cycle(offset+((cfg.sig_duration/preCycleSet.size.toDouble)*i).toInt, (cfg.sig_duration/preCycleSet.size.toDouble).toInt)
-      i+=1
-      for (turn <- preCycle) oc.add_turn(turn)
-      cycleSet += oc
-    }
-    assert(cycleSet.size == preCycleSet.size)
-    return cycleSet
-  }
+  // set up all the cycles at one intersection
+  def schedule(edge: Edge, first_offset: Double): List[Cycle] = {
+    // or standard_turn_set, or any, really.
+    val turn_groups = Cycle.grouped_left_turn_sets(edge.to)
 
-  // TODO pick the cycle that could fit our turn?
-  def next_offset(v: Vertex, default: Double): Double = {
-    val ls = cycles(v)
-    return if (ls.isEmpty)
-             default
-           else
-             ls.last.offset + ls.last.duration
+    // Schedule all cycles equally
+    val duration: Double = cfg.sig_duration / turn_groups.size.toDouble
+
+    // Find the group including the turn our starting edge leads to
+    val first_group = turn_groups.find(g => g.contains(edge.leads_to.head)).get
+    val first_cycle = new Cycle(first_offset, duration)
+    first_group.foreach(t => first_cycle.add_turn(t))
+
+    // Now schedule the rest in any order
+    var list: List[Cycle] = List(first_cycle)
+
+    var offset = first_offset + duration
+    for (group <- turn_groups if group != first_group) {
+      val c = new Cycle(offset, duration)
+      offset += duration
+      group.foreach(t => c.add_turn(t))
+      list :+= c
+    }
+
+    return list
   }
 
   // This is only used internally right now. Weight is just for ranking "4 turns
@@ -75,41 +69,40 @@ class GreenFlood(sim: Simulation) {
     def compare(other: Step) = other.weight.compare(weight)
   }
 
-  // Just one flood.  //Can we get the sentinels to help us with this whole "flood" problem?
+  // Just one flood.  // Can we get the sentinels to help us with this whole "flood" problem?
   def flood(start: Vertex) = {
     // Initialize
     val queue = new PriorityQueue[Step]()
     for (cycle <- cycles(start)) {
       // initial offset is that of the start cycle
       queue.enqueue(new Step(cycle, cycle.offset, cycle.offset))
-      visited += start
     }
+    visited += start
 
     // breadth-first search
     while (!queue.isEmpty) {
       val step = queue.dequeue
       val cycle = step.cycle
 
-      // what's the minimum delay we should have before making the next lights
-      // green?
-      // TODO the math for this could be more precise, based on accelerations
-      // and following distance delays.
-      for (turn <- cycle.turns){
-    	 val min_delay = turn.to.road.speed_limit * (turn.length + turn.to.length)
-    	 val desired_offset = (step.offset + min_delay).toInt
+      for (turn <- cycle.turns) {
+        // what's the minimum delay we should have before making the next lights
+        // green?
+        // TODO the math for this could be more precise, based on accelerations
+        // and following distance delays.
+        val min_delay = turn.to.road.speed_limit * (turn.length + turn.to.length)
+        val desired_offset = (step.offset + min_delay).toInt
 
-    	 if (!visited(turn.to.to)){
-    	 	val nextCycles = getCycles(turn.to, desired_offset)
-    	 	// schedule all the next cycles we can reach
-    	 	for (next <- nextCycles) {
-    	 		queue.enqueue(new Step(next, next.offset, step.weight + desired_offset))
-    	 	}
-    	 	cycles(turn.to.to) ++= nextCycles.toList.sortBy(c => c.offset)
-    	 	visited += turn.to.to
-    	 }
+        if (!visited(turn.to.to)) {
+          // schedule all the next cycles we can reach
+          val next_cycles = schedule(turn.to, desired_offset)
+          next_cycles.foreach(
+            next => queue.enqueue(new Step(next, next.offset, step.weight + desired_offset))
+          )
+          cycles(turn.to.to) ++= next_cycles
+          visited += turn.to.to
+        }
       }
     }
-    assert (visited.size == sim.vertices.size)
   }
 }
 
