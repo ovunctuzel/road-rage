7b8b2a8730ec2afeaad8f55bd996eb983323d11a
Dustin Carlino <dabreegster@gmail.com>
2012-11-13 19:48:17 -0600

    adding flat rectangle drawing

--- utexas/aorta/ui/MapCanvas3D.scala
+++ utexas/aorta/ui/MapCanvas3D.scala
@@ -7,8 +7,9 @@ package utexas.aorta.ui
 import com.sun.j3d.utils.behaviors.vp.OrbitBehavior
 import com.sun.j3d.utils.geometry.Sphere
 import com.sun.j3d.utils.universe.SimpleUniverse
-import javax.media.j3d.{Canvas3D, BranchGroup, BoundingSphere, AmbientLight}
-import javax.vecmath.{Point3d, Color3f}
+import javax.media.j3d.{Canvas3D, BranchGroup, BoundingSphere, AmbientLight,
+                        Shape3D, QuadArray, GeometryArray}
+import javax.vecmath.{Point3d, Color3f, Point3f}
 
 import utexas.aorta.sim.Simulation
 
@@ -35,12 +36,34 @@ class MapCanvas3D(sim: Simulation) {
 
     group.addChild(new Sphere(0.3f))
 
-    // TODO light up more
+    group.addChild(
+      new FlatRect(-30f, -10f, -5f, -5f, new Color3f(1.0f, 0f, 0f))
+    )
+
+    // TODO light up better
     val light = new AmbientLight(new Color3f(1.0f, 1.4f, .1f))
     val bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 1000.0);
     light.setInfluencingBounds(bounds)
     group.addChild(light)
 
     return group
+  }
+}
+
+class FlatRect(x1: Float, y1: Float, x2: Float, y2: Float, color: Color3f) extends Shape3D {
+  setup
+
+  def setup() = {
+    val plane = new QuadArray(4, GeometryArray.COORDINATES | GeometryArray.COLOR_3)
+    val points = List(
+      new Point3f(x1, y1, 0f),
+      new Point3f(x2, y1, 0f),
+      new Point3f(x2, y2, 0f),
+      new Point3f(x1, y2, 0f)
+    )
+    val colors = List(color, color, color, color)
+    plane.setCoordinates(0, points.toArray)
+    plane.setColors(0, colors.toArray)
+    setGeometry(plane)
   }
 }
