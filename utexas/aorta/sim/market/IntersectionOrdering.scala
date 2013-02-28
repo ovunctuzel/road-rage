85495ed9eec1d834a8d489a3657768b7af466591
Dustin Carlino <dabreegster@gmail.com>
2013-02-27 20:48:39 -0600

    different payment rules for reservations

--- utexas/aorta/sim/market/IntersectionOrdering.scala
+++ utexas/aorta/sim/market/IntersectionOrdering.scala
@@ -71,27 +71,52 @@ class AuctionOrdering[T]() extends IntersectionOrdering[T]() {
       queue = queue.tail
       Some(next)
     } else {
-      Some(process_auction(bids))
+      Some(process_auction(bids, itype))
     }
   }
 
-  private def process_auction(bids: MultiMap[T, Bid]): T = {
-    val sums = bids.keys.map(
+  private def process_auction(bids: MultiMap[T, Bid], itype: IntersectionType.Value): T =
+  {
+    val sums: List[(T, Double)] = bids.keys.map(
       t => (t, bids(t).map(_.amount).sum)
     ).toList.sortBy(_._2).reverse
 
     val winner = sums.head._1
 
-    // Direct payment... all the winners pay their full bid.
-    //bids(winner).foreach(bid => bid.who.a.wallet.spend(bid.amount, bid.who.turn))
-
-    // Proportional payment... the winner must pay the amount that the runner-up
-    // bid. Each member of the winner pays proportional to what they bid.
-    val rate = sums.tail.head._2 / sums.head._2
-    bids(winner).foreach(
-      bid => bid.who.a.wallet.spend(bid.amount * rate, bid.who.turn)
-    )
+    itype match {
+      case IntersectionType.Reservation => {
+        val winner_ticket = winner.asInstanceOf[Ticket]
+        // The one winning group pays the bid of the highest losing group who
+        // had a conflicting turn.
+        sums.tail.find(
+          pair => pair.asInstanceOf[(Ticket, Double)]._1.turn.conflicts_with(winner_ticket.turn)
+        ) match {
+          case Some(group) => {
+            pay(bids(winner), group._2)
+          }
+          // If nobody's turn conflicts, then don't pay at all!
+          case None =>
+        }
+      }
+      case _ => {
+        pay(bids(winner), sums.tail.head._2)
+      }
+    }
 
     return winner
+  }
+
+  // A group splits some cost somehow.
+  private def pay(who: Iterable[Bid], total: Double) = {
+    // Direct payment... all the winners pay their full bid.
+    //who.foreach(bid => bid.who.a.wallet.spend(bid.amount, bid.who.turn))
+
+    // Proportional payment... Each member of the winner pays proportional to
+    // what they bid.
+    val total_winners = who.map(_.amount).sum
+    val rate = total / total_winners
+    who.foreach(
+      bid => bid.who.a.wallet.spend(bid.amount * rate, bid.who.turn)
+    )
   }
 }
