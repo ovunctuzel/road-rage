7f55cafb5455d5ee0b1ba5160d0208cc2eea89fb
Dustin Carlino <dabreegster@gmail.com>
2013-07-27 18:31:54 -0400

    dont break when nothing more to replay

--- utexas/aorta/analysis/ReplayChecker.scala
+++ utexas/aorta/analysis/ReplayChecker.scala
@@ -5,9 +5,9 @@
 package utexas.aorta.analysis
 
 import utexas.aorta.sim.Simulation
-import utexas.aorta.{StateReader, StateWriter, Util}
+import utexas.aorta.{StateReader, StateWriter, Util, Common}
 
-import java.io.File
+import java.io.{File, EOFException}
 
 // TODO better name
 // Record what every agent is doing at every timestep, so that when we
@@ -24,8 +24,10 @@ abstract class ReplayChecker(sim: Simulation, ignore: Set[Int]) {
   private val fn = sim.scenario.name.replace("scenarios/", "scenarios/replay_")
   if (new File(fn).exists) {
     reader = Util.reader(fn)
+    Util.log(s"Comparing this run to a previous")
   } else {
     writer = Util.writer(fn)
+    Util.log(s"Recording this run to compare to another later")
   }
 
   private val agents = sim.scenario.agents.size
@@ -33,7 +35,7 @@ abstract class ReplayChecker(sim: Simulation, ignore: Set[Int]) {
   def handle_tick() {
     if (reader != null) {
       replay_tick()
-    } else {
+    } else if (writer != null) {
       record_tick()
     }
   }
@@ -63,26 +65,33 @@ abstract class ReplayChecker(sim: Simulation, ignore: Set[Int]) {
     // TODO fire another callback when we're simulating past where we stopped
     // last time
     var expect_id = 0
-    for (a <- sim.agents) {
-      // [expect_id, a.id)
-      for (id <- Range(expect_id, a.id) if !ignore.contains(id)) {
+    try {
+      for (a <- sim.agents) {
+        // [expect_id, a.id)
+        for (id <- Range(expect_id, a.id) if !ignore.contains(id)) {
+          val expect = reader.double
+          if (expect != ReplayChecker.MISSING) {
+            difference(id, expect, ReplayChecker.MISSING)
+          }
+        }
+        if (!ignore.contains(a.id)) {
+          val expect = reader.double
+          expect_id = a.id + 1
+          if (expect != a.characterize_choice) {
+            difference(a.id, expect, a.characterize_choice)
+          }
+        }
+      }
+      for (id <- Range(expect_id, agents + 1) if !ignore.contains(id)) {
         val expect = reader.double
         if (expect != ReplayChecker.MISSING) {
           difference(id, expect, ReplayChecker.MISSING)
         }
       }
-      if (!ignore.contains(a.id)) {
-        val expect = reader.double
-        expect_id = a.id + 1
-        if (expect != a.characterize_choice) {
-          difference(a.id, expect, a.characterize_choice)
-        }
-      }
-    }
-    for (id <- Range(expect_id, agents + 1) if !ignore.contains(id)) {
-      val expect = reader.double
-      if (expect != ReplayChecker.MISSING) {
-        difference(id, expect, ReplayChecker.MISSING)
+    } catch {
+      case _: EOFException => {
+        Util.log(s"Nothing more to replay at ${Common.tick}. Ignoring differences.")
+        reader = null
       }
     }
   }
