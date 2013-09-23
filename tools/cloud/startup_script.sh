296c579811c9e23ef4f26e97d182e55c3b5126f9
Dustin Carlino <dabreegster@gmail.com>
2013-09-22 20:22:40 -0500

    autoshutdown when done

--- tools/cloud/startup_script.sh
+++ tools/cloud/startup_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Startup script each instance executes to setup packages and launch the script to run experiments.
 
+GS=gs://aorta/`hostname`-
+
 # Install java and scala
 sudo apt-get install -y openjdk-6-jre libjansi-java
 wget http://scala-lang.org/files/archive/scala-2.10.2.deb
@@ -11,7 +13,13 @@ gsutil cp gs://aorta/aorta.tgz .  # TODO force...
 mkdir aorta
 cd aorta
 tar xzf ../aorta.tgz
+./tools/cloud/upload_gs.sh ${GS}-status 'Compiling'
 ./recompile
 
 # Run the experiment
-./tools/analyze_routes gs://aorta/`hostname`-
+./tools/analyze_routes $GS
+# TODO do it multiple times to amortize compilation time? can't overwrite results
+
+# When we're done, shutdown
+./tools/cloud/upload_gs.sh ${GS}-status 'Done'
+gcutil deleteinstance --force `hostname`
