fa13ab7227cb6e1a8ed5716e54773cc6bde55b9e
Dustin Carlino <dabreegster@gmail.com>
2013-11-28 12:19:38 -0600

    actually, move dist formulas to common/algos

--- utexas/aorta/map/Coordinate.scala
+++ utexas/aorta/map/Coordinate.scala
@@ -5,6 +5,7 @@
 package utexas.aorta.map
 
 import utexas.aorta.common.{StateWriter, StateReader}
+import utexas.aorta.common.algorithms.Distance
 
 /*
  * A vital note about coordinate systems:
@@ -39,35 +40,8 @@ object Coordinate {
   private val earth_radius = 6378100.0
 
   // use Graph.world_to_gps to get original GPS coordinates first.
-  def gps_dist_in_meters(c1: Coordinate, c2: Coordinate) = haversine_dist(c1, c2)
-
-  // Haversine formula (slow, but accurate) from
-  // http://www.movable-type.co.uk/scripts/latlong.html
-  private def haversine_dist(c1: Coordinate, c2: Coordinate): Double = {
-    val lon1 = math.toRadians(c1.x)
-    val lon2 = math.toRadians(c2.x)
-    val lat1 = math.toRadians(c1.y)
-    val lat2 = math.toRadians(c2.y)
-
-    val dLat = lat2 - lat1
-    val dLon = lon2 - lon1
-    val a = math.pow(math.sin(dLat / 2), 2) +
-    	      math.pow(math.sin(dLon / 2), 2) * math.cos(lat1) * math.cos(lat2)
-    val c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    return earth_radius * c
-  }
-
-  // Equirectangular approximation (fast, still should be pretty accurate) from
-  // http://www.movable-type.co.uk/scripts/latlong.html
-  private def equirectangular_dist(c1: Coordinate, c2: Coordinate): Double = {
-    val lon1 = math.toRadians(c1.x)
-    val lon2 = math.toRadians(c2.x)
-    val lat1 = math.toRadians(c1.y)
-    val lat2 = math.toRadians(c2.y)
-    val x = (lon2 - lon1) * math.cos((lat1 + lat2) / 2)
-    val y = lat2 - lat1
-    return earth_radius * math.sqrt(math.pow(x, 2) + math.pow(y, 2))
-  }
+  def gps_dist_in_meters(c1: Coordinate, c2: Coordinate) =
+    Distance.haversine_dist(c1.x, c1.y, c2.x, c2.y)
 
   def unserialize(r: StateReader) = Coordinate(r.double, r.double)
 }
