c61f8c1cb4d56afe89fe56e9003edad5e9696548
Dustin Carlino <dabreegster@gmail.com>
2013-12-01 19:31:29 -0600

    extracting a PQ class from A*

--- utexas/aorta/common/algorithms/AStar.scala
+++ utexas/aorta/common/algorithms/AStar.scala
@@ -26,33 +26,21 @@ object AStar {
     // Best cost so far
     val costs = new mutable.HashMap[T, (Double, Double)]()
 
-    case class Step(state: T) {
-      lazy val heuristic = calc_heuristic(state, goal)
-      def cost = add_cost(costs(state), heuristic)
-    }
-    val ordering = Ordering[(Double, Double)].on((step: Step) => step.cost).reverse
+    val open = new PriorityQueue[T]()
     val ordering_tuple = Ordering[(Double, Double)].on((pair: (Double, Double)) => pair)
 
-    // Priority queue grabs highest priority first, so reverse to get lowest
-    // cost first.
-    val open = new mutable.PriorityQueue[Step]()(ordering)
-    // Used to see if we've already added a road to the queue
-    val open_members = new mutable.HashSet[T]()
-
     costs(start) = (0, 0)
-    open.enqueue(Step(start))
-    open_members += start
+    open.insert(start, calc_heuristic(start, goal))
     // Indicate start in backrefs by not including it
 
     while (open.nonEmpty) {
-      val current = open.dequeue()
-      visited += current.state
-      open_members -= current.state
+      val current = open.shift()
+      visited += current
 
-      if (current.state == goal) {
+      if (current == goal) {
         // Reconstruct the path
         val path = new mutable.ListBuffer[T]()
-        var pointer: Option[T] = Some(current.state)
+        var pointer: Option[T] = Some(current)
         while (pointer.isDefined) {
           path.prepend(pointer.get)
           // Clean as we go to break loops
@@ -61,17 +49,16 @@ object AStar {
         // Exclude 'start'
         return path.tail.toList
       } else {
-        for (next_state <- successors(current.state)) {
+        for (next_state <- successors(current)) {
           val tentative_cost = add_cost(
-            costs(current.state), calc_cost(current.state, next_state, costs(current.state))
+            costs(current), calc_cost(current, next_state, costs(current))
           )
-          if (!visited.contains(next_state) && (!open_members.contains(next_state) || ordering_tuple.lt(tentative_cost, costs(next_state)))) {
-            backrefs(next_state) = current.state
+          if (!visited.contains(next_state) && (!open.contains(next_state) || ordering_tuple.lt(tentative_cost, costs(next_state)))) {
+            backrefs(next_state) = current
             costs(next_state) = tentative_cost
             // TODO if they're in open_members, modify weight in the queue? or
             // new step will clobber it. fine.
-            open.enqueue(Step(next_state))
-            open_members += next_state
+            open.insert(next_state, add_cost(tentative_cost, calc_heuristic(next_state, goal)))
           }
         }
       }
@@ -79,4 +66,30 @@ object AStar {
 
     throw new Exception("Couldn't A* from " + start + " to " + goal)
   }
+}
+
+// TODO generalize score.
+class PriorityQueue[T]() {
+  private case class Item(item: T, weight: (Double, Double))
+
+  private val pq = new mutable.PriorityQueue[Item]()(
+    Ordering[(Double, Double)].on((item: Item) => item.weight).reverse
+  )
+  private val members = new mutable.HashSet[T]()
+
+  def insert(item: T, weight: (Double, Double)) {
+    pq.enqueue(Item(item, weight))
+    members += item
+  }
+
+  def shift(): T = {
+    val item = pq.dequeue().item
+    members -= item // TODO not true if there are multiples!
+    return item
+  }
+
+  // TODO have a change_weight.
+
+  def contains(item: T) = members.contains(item)
+  def nonEmpty = pq.nonEmpty
 }
