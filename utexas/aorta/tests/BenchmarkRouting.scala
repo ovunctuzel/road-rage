b4291be312ccb53abcfc6a46a67e9de86282b191
Dustin Carlino <dabreegster@gmail.com>
2013-12-02 19:15:00 -0600

    removed experimental 'strand router'. that didnt last long.

--- utexas/aorta/tests/BenchmarkRouting.scala
+++ utexas/aorta/tests/BenchmarkRouting.scala
@@ -10,30 +10,33 @@ import utexas.aorta.common.{Util, Common, cfg}
 
 object BenchmarkRouting {
   def main(args: Array[String]) = {
-    val rounds = 500
+    val rounds = 1000
 
     val sim = Util.process_args(args)
     val rng = new utexas.aorta.common.RNG()
 
-    val ch_router = new CHRouter(sim.graph)
-    val congestion_router = new CongestionRouter(sim.graph)
-    val strand_router = new CompressedGraph(sim.graph)
-    val sum = Array.fill[Double](3)(0.0)
+    val routers = List(
+      (new CHRouter(sim.graph), "ch"),
+      (new CongestionRouter(sim.graph), "congestion_a*")
+    )
+    val sum_times = Array.fill[Double](routers.size)(0.0)
 
     for (i <- 1 until rounds) {
       val from = rng.choose(sim.graph.directed_roads)
       val to = rng.choose(sim.graph.directed_roads)
 
       if (i % (rounds / 100) == 0) {
-        println(s"round $i / $rounds")
+        Util.log(s"round $i / $rounds")
       }
-      for (((router, name), idx) <- List((ch_router, "ch"), (congestion_router, "cong"), (strand_router, "strand")).zipWithIndex) {
+      for (((router, name), idx) <- routers.zipWithIndex) {
         val t = Common.timer(name)
         router.path(from, to, 0.0)
-        sum(idx) += t.so_far
+        sum_times(idx) += t.so_far
       }
     }
-    println(s"\n\nCH: ${sum(0) / rounds}, cong: ${sum(1) / rounds}, strand: ${sum(2) / rounds}")
-    println(s"total for $rounds... CH: ${sum(0)}, cong: ${sum(1)}, strand: ${sum(2)}")
+    Util.log("\n\n")
+    for (((_, name), idx) <- routers.zipWithIndex) {
+      Util.log(s"$name: ${sum_times(idx)}s total, ${sum_times(idx) / rounds}s per path")
+    }
   }
 }
