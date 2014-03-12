ff0eda6decacf81bfbe16f0399ff6498f3263274
Dustin Carlino <dabreegster@gmail.com>
2014-03-11 19:56:21 -0500

    quasiquotes are weird

--- macros/Serialization.scala
+++ macros/Serialization.scala
@@ -44,17 +44,34 @@ object Mappable {
         q"w.bool(t.$name)"
       } else if (field_type <:< typeOf[Array[_]] || field_type <:< typeOf[List[_]]) {
         val type_param = field_type.asInstanceOf[TypeRef].args.head
-        q"w.int(t.$name.size)"
-        /*q"""
+        // TODO For some reason, quasiquoting type_param directly blows up
+        val inner = type_param.toString match {
+          case "utexas.aorta.sim.make.MkAgent" => q"MkAgent.do_magic(obj, w)"
+          case "utexas.aorta.sim.make.MkIntersection" => q"MkIntersection.do_magic(obj, w)"
+          case "utexas.aorta.common.RoadID" => q"w.int(obj.int)"
+        }
+        q"""
           w.int(t.$name.size)
           for (obj <- t.$name) {
-            $type_param.do_magic(obj, w)
+            $inner
           }
-        """*/
+        """
+      } else if (field_type.toString.endsWith(".Value")) {
+        // TODO hacky way to detect enumerations
+        q"w.int(t.$name.id)"
+      } else if (field_type.toString.endsWith("ID")) {
+        // TODO hacky way to detect AnyVals...
+        q"w.int(t.$name.int)"
+      // TODO hacky way of getting around quasiquoting
+      } else if (field_type.toString == "utexas.aorta.sim.make.SystemWalletConfig") {
+        q"SystemWalletConfig.do_magic(t.$name, w)"
+      } else if (field_type.toString == "utexas.aorta.sim.make.MkRoute") {
+        q"MkRoute.do_magic(t.$name, w)"
+      } else if (field_type.toString == "utexas.aorta.sim.make.MkWallet") {
+        q"MkWallet.do_magic(t.$name, w)"
       } else {
-        // TODO Enumerations are hard to detect
-        println(s"$name is $field_type")
-        q"w.string(t.$name.toString)"
+        // TODO error messages don't actually show up
+        c.abort(c.enclosingPosition, s"Dunno how to serialize $field_type")
       }
     })
 
@@ -62,8 +79,6 @@ object Mappable {
       new Mappable[$tpe] {
         def magic_save(t: $tpe, w: MagicWriter) {
           ..$per_field
-          val result = "foo"
-          println(result)
         }
       }
     """ }
